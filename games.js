2bcd55384f06379d11ac4e710c13db2922e85c73
kitajchuk <bk@kitajchuk.com>
2025-03-29 13:29:01 -0700

    minify dist game json

---
+++ games.js
@@ -0,0 +1,20 @@
+const fs = require( "fs" );
+const shell = require( "shelljs" );
+
+// Source games data copied to `dist`
+const shellCmd = "find ./dist/games -type f";
+const shellOpts = { silent: true };
+
+// Shell out to `find` for quick recurse
+const files = shell
+  .exec(shellCmd, shellOpts)
+  .stdout.split("\n")
+  .filter((f) => /\.(json)$/.test(f));
+
+// Minify JSON for each game
+files.forEach((file) => {
+  const json = JSON.parse(fs.readFileSync(file));
+  const minified = JSON.stringify(json);
+  console.log(`Minified game JSON: ${file}`);
+  fs.writeFileSync(file, minified);
+});
