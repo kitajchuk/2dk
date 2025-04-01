25d28530b1a2a1a6fa9d1d3919e9d4b1031ce1a4
kitajchuk <bk@kitajchuk.com>
2025-03-31 19:29:27 -0700

    collidascope

--- studio/games/pk/sw.js
+++ studio/games/pk/sw.js
@@ -1,5 +1,5 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers
-const CACHE_NAME = "v35";
+const CACHE_NAME = "v36";
 const CACHE_URLS = [
     "",
     "index.html",
