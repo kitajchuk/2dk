815b6e442879d37f10dcbad94d3594164ba66625
kitajchuk <bk@kitajchuk.com>
2025-04-07 16:44:08 -0700

    toronbo shiz

--- studio/games/la/sw.js
+++ studio/games/la/sw.js
@@ -1,5 +1,5 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers
-const CACHE_NAME = "v720";
+const CACHE_NAME = "v757";
 const CACHE_URLS = [
     "",
     "index.html",
