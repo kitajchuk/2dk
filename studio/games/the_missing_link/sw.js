3e8d435b1acfb23795cbb9ecad3df4803edb430b
kitajchuk <bk@kitajchuk.com>
2025-12-27 16:26:54 -0800

    bump sw for demos

--- studio/games/the_missing_link/sw.js
+++ studio/games/the_missing_link/sw.js
@@ -1,5 +1,5 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers
-const CACHE_NAME = "v195";
+const CACHE_NAME = "v196";
 const CACHE_URLS = [
     "",
     "index.html",
