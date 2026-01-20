13779ca00a72983af09b562af7844dc9233adf38
kitajchuk <bk@kitajchuk.com>
2026-01-20 11:39:05 -0800

    bumps

--- studio/games/the_missing_link/sw.js
+++ studio/games/the_missing_link/sw.js
@@ -1,5 +1,5 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers
-const CACHE_NAME = "v905";
+const CACHE_NAME = "v906";
 const CACHE_URLS = [
     "",
     "index.html",
