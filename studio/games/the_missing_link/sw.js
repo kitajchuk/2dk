e3f74189b7b2fae66ecc9d825e37785a6ec31e22
kitajchuk <bk@kitajchuk.com>
2026-01-20 15:28:54 -0800

    tile change

--- studio/games/the_missing_link/sw.js
+++ studio/games/the_missing_link/sw.js
@@ -1,5 +1,5 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers
-const CACHE_NAME = "v921";
+const CACHE_NAME = "v922";
 const CACHE_URLS = [
     "",
     "index.html",
