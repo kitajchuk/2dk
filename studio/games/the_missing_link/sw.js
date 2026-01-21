4f670c3aeb9ee059fd2cbbd7d414ab87494271cf
kitajchuk <bk@kitajchuk.com>
2026-01-20 21:53:26 -0800

    sewers

--- studio/games/the_missing_link/sw.js
+++ studio/games/the_missing_link/sw.js
@@ -1,5 +1,5 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers
-const CACHE_NAME = "v927";
+const CACHE_NAME = "v955";
 const CACHE_URLS = [
     "",
     "index.html",
@@ -47,6 +47,7 @@ const CACHE_URLS = [
     "assets/sounds/sfx-sword.mp3",
     "assets/sounds/sfx-throw.mp3",
     "assets/scenes/logo.png",
+    "maps/beneath-the-cathedral.json",
     "maps/beneath-the-grave.json",
     "maps/beneath-the-moblin-hideout.json",
     "maps/beneath-the-rooster.json",
