210e1214e54befc83bf568690bcfab691e749d0b
kitajchuk <bk@kitajchuk.com>
2026-02-04 22:07:00 -0800

    sq

--- studio/games/the_missing_link/sw.js
+++ studio/games/the_missing_link/sw.js
@@ -1,5 +1,5 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers
-const CACHE_NAME = "v1281";
+const CACHE_NAME = "v1282";
 const CACHE_URLS = [
     "",
     "index.html",
