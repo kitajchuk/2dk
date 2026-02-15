0228bcb0b02f6ec5b46a20cce705c6fd0711e0c6
kitajchuk <bk@kitajchuk.com>
2026-02-15 09:50:34 -0800

    fix it

--- studio/games/the_missing_link/sw.js
+++ studio/games/the_missing_link/sw.js
@@ -1,5 +1,5 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers
-const CACHE_NAME = "v1408";
+const CACHE_NAME = "v1409";
 const CACHE_URLS = [
     "",
     "index.html",
