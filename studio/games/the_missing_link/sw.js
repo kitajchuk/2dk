4ef8e9f2c935534c3c08ce68e9cd99301af54132
kitajchuk <bk@kitajchuk.com>
2026-02-22 15:31:17 -0800

    more maps

--- studio/games/the_missing_link/sw.js
+++ studio/games/the_missing_link/sw.js
@@ -1,5 +1,5 @@
 // https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers
-const CACHE_NAME = "v1716";
+const CACHE_NAME = "v1750";
 const CACHE_URLS = [
     "",
     "index.html",
@@ -72,6 +72,7 @@ const CACHE_URLS = [
     "maps/tunnel-crypt.json",
     "maps/under-the-plateau.json",
     "maps/untended-graveyard.json",
+    "maps/wooded-passage.json",
 ];
 
 
